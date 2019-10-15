a8fa04581cc04b7edfb3bff7e0b32124e305c3cd
Andrej Beliančin <3210ado@gmail.com>
2019-10-15 23:42:03 +0200

    Constants from named_constants to basic constants. TO DO to future

--- constants.py
+++ constants.py
@@ -1,18 +1,52 @@
 import named_constants
 
-class Constants(named_constants.Constants):
-    class Datasets(named_constants.Constants):
-        MNIST, FASHION_MNIST, CIFAR_10 = range(3)
+class MetaConst(type):
+    def __getattr__(cls, key):
+        return cls[key]
 
-    class Paths(named_constants.Constants):
+    def __setattr__(cls, key, value):
+        raise TypeError
+
+class Const(metaclass=MetaConst):
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        raise TypeError
+
+class Constants(Const):
+    class Datasets(Const):
+        MNIST = 'MNIST'
+        FASHION_MNIST = 'FASHION_MNIST'
+        CIFAR_10 = 'CIFAR_10'
+
+    class Paths(Const):
         OUTPUT_DIRECTORY = './output_dir/'
 
-    class GpuConfig(named_constants.Constants):
+    class GpuConfig(Const):
         DEFAULT, LIMIT, CONTINOUS_GROWTH = range(3)
 
-    class Models(named_constants.Constants):
+    class Models(Const):
+        AUTOENCODER = 'autoencoder'
+        CLASSIFIER = 'classifier'
+        AUTO_CLASSIFIER = 'auto_classifier'
         ENCODED_OUT = 'ENCODED_OUT'
         DECODED_OUT = 'DECODED_OUT'
         CLASSIFIER_OUT = 'CLASSIFIER_OUT'
         INPUT_SHAPE = 'INPUT_SHAPE'
 
+    class Experiments(Const):
+        PATIENCE = 20
+        AUTOENCODER = 'AUTOENCODER'
+        CLASSIFIER = 'CLASSIFIER'
+        AE_CL_AU_L_OFF = 'AUTOENCODER_CLASSIFIER_AUTOENCODER_LAYERS_OFF'
+        AE_CL_AU_L_ON = 'AUTOENCODER_CLASSIFIER_AUTOENCODER_LAYERS_ON'
+
+    class Metrics(Const):
+        ACCURACY = 'acc'
+        LOSS = 'loss'
+        VAL_LOSS = 'val_loss'
+        VAL_ACCURACY = 'val_accuracy'
+
+    class Logger(Const):
+        ROOT_KEY = 'modellist'
