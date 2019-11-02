30319f8eb92cb177d0295e1f72af3b663d0406c0
Andrej Beliančin <3210ado@gmail.com>
2019-11-02 19:04:29 +0100

    IS_COLAB  variable added

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
-from constants import Constants
+import constants
 
 class Config():
+    IS_COLAB = False
+    try:
+        import google.colab
+        IS_COLAB = True
+    except:
+        pass
+
     IS_LINUX = False
     from sys import platform
-
     if platform == "linux" or platform == "linux2":
         IS_LINUX = True
 
-    GPU_CONFIG = Constants.GpuConfig.LIMIT
+    DISABLE_GPU = False
+    if IS_COLAB:
+        GPU_CONFIG = constants.GpuConfig.CONTINOUS_GROWTH
+    else:
+        GPU_CONFIG = constants.GpuConfig.LIMIT
+    GPU_LIMIT = 768
+
