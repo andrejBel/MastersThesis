06718d31a8664578f6708e309973b872b602f06f
Andrej Beliančin <3210ado@gmail.com>
2019-11-02 23:09:31 +0100

    get all subclasses

--- global_functions.py
+++ global_functions.py
@@ -97,3 +97,12 @@ def remove_file(filepath):
         os.remove(filepath)
     else:
         print("Error: %s file not found" % filepath)
+
+def get_all_subclasses(cls):
+    all_subclasses = []
+
+    for subclass in cls.__subclasses__():
+        all_subclasses.append(subclass)
+        all_subclasses.extend(get_all_subclasses(subclass))
+
+    return all_subclasses
