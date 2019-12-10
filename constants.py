0950e3df81a2eb458a237e42fe8602204e35a47f
Andrej Beliančin <3210ado@gmail.com>
2019-12-10 22:05:11 +0100

    constants - paths for model ssaving changed

--- constants.py
+++ constants.py
@@ -16,12 +16,16 @@ class Const(metaclass=MetaConst):
 
 class Paths(Const):
     OUTPUT_DIRECTORY = './output_dir/'
+    JSON_FILE_FORMAT = '.json'
 
 
 class Datasets(Const):
-    MNIST = 'MNIST'
-    FASHION_MNIST = 'FASHION_MNIST'
-    CIFAR_10 = 'CIFAR_10'
+    MNIST = 'mnist'
+    MNIST_ABBREV = 'm'
+    FASHION_MNIST = 'fashion_mnist'
+    FASHION_MNIST_ABBREV = 'fm'
+    CIFAR_10 = 'cifar_10'
+    CIFAR_10_ABBREV = 'c10'
 
 
 class GpuConfig(Const):
@@ -32,28 +36,43 @@ class Models(Const):
     AUTOENCODER = 'autoencoder'
     CLASSIFIER = 'classifier'
     AUTO_CLASSIFIER = 'auto_classifier'
-    ENCODED_OUT = 'ENCODED_OUT'
-    DECODED_OUT = 'DECODED_OUT'
-    CLASSIFIER_OUT = 'CLASSIFIER_OUT'
-    INPUT_SHAPE = 'INPUT_SHAPE'
+    ENCODED_OUT = 'encoded_out'
+    DECODED_OUT = 'decoded_out'
+    CLASSIFIER_OUT = 'classifier_out'
+    INPUT_SHAPE = 'input_shape'
 
 
 class ExperimentsPaths(Const):
     class FashionMnist:
-        FASHION_MNIST_POSTFIX = '_fm.json'
-        AUTOENCODER = Paths.OUTPUT_DIRECTORY + 'autoencoder' + FASHION_MNIST_POSTFIX  # 1
-        CLASSIFIER = Paths.OUTPUT_DIRECTORY + 'classifier' + FASHION_MNIST_POSTFIX  # 2
-        AUTOENCODER1_CLASSIFIER1_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_off' + FASHION_MNIST_POSTFIX  # 3
-        AUTOENCODER1_CLASSIFIER1_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_on' + FASHION_MNIST_POSTFIX  # 4
-        AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_off' + FASHION_MNIST_POSTFIX  # 5
-        AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_on' + FASHION_MNIST_POSTFIX  # 6
-        AUTO_CLASSIFIER = Paths.OUTPUT_DIRECTORY + 'autoclassifier' + FASHION_MNIST_POSTFIX #7
-        CLASSIFIER_LAYERS_OFF = Paths.OUTPUT_DIRECTORY + 'classifier_layers_off' + FASHION_MNIST_POSTFIX #8
+        class BasicModelBuilder:
+            BASIC_MODEL_BUILDER_POSTFIX = '_bmp'
+            POSTFIX = BASIC_MODEL_BUILDER_POSTFIX + '_' + Datasets.FASHION_MNIST_ABBREV + Paths.JSON_FILE_FORMAT
+            AUTOENCODER = Paths.OUTPUT_DIRECTORY + 'autoencoder' + POSTFIX  # 1
+            CLASSIFIER_EN_LAYERS_ON = Paths.OUTPUT_DIRECTORY + 'classifier_encoder_layers_on' + POSTFIX  # 2
+            CLASSIFIER_EN_LAYERS_OFF = Paths.OUTPUT_DIRECTORY + 'classifier_encode_layers_off' + POSTFIX  # 8
+            AUTOENCODER1_CLASSIFIER1_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_off' + POSTFIX  # 3
+            AUTOENCODER1_CLASSIFIER1_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_on' + POSTFIX  # 4
+            AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_off' + POSTFIX  # 5
+            AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_on' + POSTFIX  # 6
+            AUTO_CLASSIFIER = Paths.OUTPUT_DIRECTORY + 'autoclassifier' + POSTFIX #7
+
+        class BasicModelBuilderWithAveragePoolingWithoutDenseBuilder:
+            BASIC_MODEL_BUILDER_WITHOUT_DENSE_POSTFIX = '_bmwawt'
+            POSTFIX = BASIC_MODEL_BUILDER_WITHOUT_DENSE_POSTFIX + '_' + Datasets.FASHION_MNIST_ABBREV + Paths.JSON_FILE_FORMAT
+
+            CLASSIFIER_EN_LAYERS_ON = Paths.OUTPUT_DIRECTORY + 'classifier_encoder_layers_on' + POSTFIX  # 2
+            CLASSIFIER_EN_LAYERS_OFF = Paths.OUTPUT_DIRECTORY + 'classifier_encode_layers_off' + POSTFIX  # 8
+            AUTOENCODER1_CLASSIFIER1_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_off' + POSTFIX  # 3
+            AUTOENCODER1_CLASSIFIER1_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder1_classifier1_auto_l_on' + POSTFIX  # 4
+            AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_OFF = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_off' + POSTFIX  # 5
+            AUTOENCODER_TRAINED_CLASSIFIER_AUTO_L_ON = Paths.OUTPUT_DIRECTORY + 'autoencoder_trained_classifier_auto_l_on' + POSTFIX  # 6
 
     class Mnist:
-        MNIST_POSTFIX = '_m.json'
-        AUTOENCODER = Paths.OUTPUT_DIRECTORY + 'autoencoder' + MNIST_POSTFIX  # 1
-        CLASSIFIER = Paths.OUTPUT_DIRECTORY + 'classifier' + MNIST_POSTFIX  # 2
+        class BasicModelBuilder:
+            BASIC_MODEL_BUILDER_POSTFIX = '_bmp'
+            POSTFIX = BASIC_MODEL_BUILDER_POSTFIX + '_' + Datasets.MNIST_ABBREV + Paths.JSON_FILE_FORMAT
+            AUTOENCODER = Paths.OUTPUT_DIRECTORY + 'autoencoder' + POSTFIX  # 1
+            CLASSIFIER = Paths.OUTPUT_DIRECTORY + 'classifier' + POSTFIX  # 2
 
 
 class Metrics(Const):
